e8ac73a85f436f11a79390d59785a70c8c85de58
inakleinbottle <41870650+inakleinbottle@users.noreply.github.com>
2019-10-21 15:49:57 +0100

    Various updates

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
 # Montyhall
-Simulation runner for Monty hall type problems
+This package provides tools for running simulations of the Monty Hall problem and similar problems and determining how many of those simulations are successful.
+
+## Background
+The Monty Hall gameshow has a simple premise. There are three doors,
+behind one of which is a prize. The contestant is asked to choose one
+door. The host will then reveal another door that does not conceal the
+prize. The contestant is then given the opportunity to swap.
+
+Conventional thinking would suggest that at this stage, each of the
+remaining two doors has an equal probability of concealing the prize,
+but this is not the case. Indeed, at the beginning, each door has an
+equal probability, one in three (1/3), of being the prize door. Say
+the contestant chooses door A. Then door A has a 1/3 probability of
+being correct while, combined, the probability that either B or C is
+correct is 2/3. The host now reveals one of the remaining doors to be
+false, say door B. Now the probability that one of door B or C is the
+prize door is still 2/3, but we have the additional information that
+the probability that door B is correct is 0, so all of this 2/3
+probability that B or C is correct must lie with door C. Thus the
+contestant is twice as likely to win if they swap to door C.
+
+## Example - The classical Monty Hall problem
+To run experiments for the classical Monty Hall problem, we can used the pre-defined `CLASSICAL_MONTY_HALL` experiment object.
+```python3
+>>> from montyhall import CLASSICAL_MONTY_HALL, always_swap, never_swap
+>>> CLASSICAL_MONTY_HALL.run_simulations(always_swap)
+0.669
+>>> CLASSICAL_MONTY_HALL.run_simulations(never_swap)
+0.315
+```
+After the default (1000) number of experiments, the estimated probability of success using the `always_swap` strategy is roughly 0.666 (2/3), while that of the `never_swap` strategy is roughly 0.333 (1/3), as expected. (Note that the simulations have a random element, so your results may differ.)
+
+## Example - A variation on the classical problem
+The `MontyHallExperiment` class can be used to run simulations for variations of the classical problem. For example, if instead we have four doors carrying equal probabilities then we can run the corresponding simulations as follows.
+```python3
+>>> from montyhall import MontyHallExperiment, always_swap, never_swap
+>>> exp = MontyHallExperiment(number_of_doors=4)
+>>> exp.run_simulations(always_swap)
+0.612
+>>> exp.run_simulations(never_swap)
+0.28
+```
+Alternatively, we can control the probability that a given door is the correct door in the experiment by providing the doors explicitly.
+```python3
+>>> from montyhall import always_swap, never_swap, MontyHallExperiment, Door
+>>> exp = MontyHallExperiment(Door(0.5), Door(0.3), Door(0.2))
+>>> exp.run_simulations(always_swap)
+0.672
+>>> exp.run_simulations(never_swap)
+0.332
+```
+
+## Installation
+At present, the package can only be installed manually.
+
+Clone the repository from github using
+```
+git clone https://github.com/inakleinbottle/Montyhall.git
+```
+
+
+## License
+This package is shared under the MIT license (see LICENSE.txt).
+
+
+
+
+
--- montyhall.py
+++ montyhall.py
@@ -1,30 +1,86 @@
-from itertools import accumulate
+"""
+Package for simulating the Monty Hall game with various strategies.
+
+This package provides the `MontyHallExperiment` class, that is used to
+run a number of simulations on a Monty Hall type problem and determine,
+empirically, the probability of success using a given strategy. The
+classical Monty Hall problem is provided as `CLASSICAL_MONTY_HALL`.
+This is a pre-instantiated experiment with three doors carrying equal
+probabilities.
+
+The strategy should be a function that takes two arguments, the
+list of available doors and a list of past selections. The function
+should return the number, from the doors list, of the selected door.
+Two standard strategies are implemented here, the always swap stategy
+(optimum solution for the classical problem) and the never swap
+strategy.
+"""
 import random
-import concurrent.futures as cf
 import functools
+import inspect
+import time
+from concurrent.futures import ProcessPoolExecutor
+from itertools import accumulate
+
+__all__ = [
+    "Door", 
+    "MontyHallExperiment", 
+    "CLASSICAL_MONTY_HALL", 
+    "always_swap", 
+    "never_swap",
+]
 
 class Door:
+    """
+    Door for use in a Monty Hall experiment.
+
+    Each door can be given an custom probability if this differs from
+    the classical case of equal probabilities.
+    """
 
     def __init__(self, probability=None):
         if probability is not None:
             assert 0 < probability < 1, "Probability must be between 0 and 1"
         self.probability = probability
 
+    def __repr__(self):
+        return f"Door(probabilty={self.probability})"
+
+    def __str__(self):
+        return f"Door with probability {self.probability}"
 
 
 def experiment(doors, strategy, correct):
+    """
+    Function that runs the actual experiment.
+
+    A selection is first chosen at random. Then, whilst there are at
+    least 3 doors remaining, the host reveals one incorrect door that
+    is not the door most recently selected. Then the contestant's
+    strategy is applied to obtain the next selection.
+
+    Once only two doors remain, the experiment returns True if the most
+    recent selection was the correct door, and False otherwise.
+    """
     selections = [random.choice(doors)]
     while len(doors) > 2:
         elim = random.choice([d for d in doors if not d == correct
                               if not d == selections[-1]])
-        doors.pop(elim)
+        doors.remove(elim)
         selections.append(strategy(doors, selections))
     return selections[-1] == correct
         
 
 class MontyHallExperiment:
+    """
+    Monty Hall type problem experiment.
 
-    def __init__(self, *doors, number_of_doors=None):
+    The available doors can be specified individually when creating an
+    experiment object, or by using the keyword-only number_of_doors
+    parameter. 
+    """
+
+    def __init__(self, *doors, number_of_doors=None, experiment=experiment):
         if number_of_doors and doors:
             raise ValueError("Number of doors and individual doors cannot be"
                              " specified together")
@@ -33,6 +89,7 @@ class MontyHallExperiment:
             doors = [Door(prob) for _ in range(number_of_doors)]
         else:
             doors = list(doors)
+            number_of_doors = len(doors)
 
         existing = sum(d.probability for d in doors 
                        if d.probability is not None)
@@ -47,25 +104,57 @@ class MontyHallExperiment:
 
         self.doors = doors
 
+        self.experiment = experiment
+
     def get_correct_doors(self, number):
+        """
+        Generate the sequence of correct doors randomly according to
+        the probability assigned to each door.
+
+        This is a generator function.
+        """
         cum_weights = list(accumulate(d.probability for d in self.doors))
         for _ in range(number):
             rand_no = random.random()
             yield min(i for i, w in enumerate(cum_weights)
                       if rand_no < w)
 
-    def run_simulations(self, strategy, number=1000):
+    def run_simulations(self, strategy, number=1000, max_workers=None):
         """
         Run the number of simulations.
+
+        Simulations are run concurrently in individual processes. For
+        this reason, the provided strategy must be a function and not a
+        lambda. The maximum numbers of worker processes can be 
+        controlled by the `max_workers` parameter. By default, this is
+        the number of cores of the host computer.
+
+        Returns the proportion of experiments that were successful.
         """
+        start = time.time()
+        assert inspect.isfunction(strategy), "strategy must be a function"
         doors = list(range(len(self.doors)))
-        func = functools.partial(experiment, doors, strategy) 
-        with cf.ProcessPoolExecutor() as pool:
+        func = functools.partial(self.experiment, doors, strategy) 
+        with ProcessPoolExecutor(max_workers=max_workers) as pool:
             results = pool.map(func, self.get_correct_doors(number))
             successes = sum(1 for r in results if r)
+        print(f"Ran {number} simulations in {time.time() - start} seconds")
         return successes / number
 
 
 def always_swap(doors, past_selections):
-    return min(i for i in doors if i not in past_selections)
+    """
+    Strategy that always swaps when given a chance.
 
+    This is the optimum strategy for the classical Monty Hall problem.
+    """
+    return min(i for i in doors if not i == past_selections[-1])
+
+def never_swap(doors, past_selections):
+    """
+    Strategy that never swaps when given a chance.
+    """
+    return past_selections[-1]
+
+
+CLASSICAL_MONTY_HALL = MontyHallExperiment(number_of_doors=3)
